cc2d3afa31fda82fd1821d0ea42444a87c776a1f
melwin <114172287+itzTedx@users.noreply.github.com>
2025-07-21 14:24:17 +0400

    Update next.config.ts to enhance image handling, add experimental package imports optimization, and configure TypeScript and ESLint settings. Update package.json and pnpm-lock.yaml to include new Radix UI Tabs dependency.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    formats: ["image/avif", "image/webp"],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
+    minimumCacheTTL: 60,
+  },
+
+  experimental: {
+    optimizePackageImports: ["lucide-react"],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
 };
 
 export default nextConfig;
