179686d4b1c9e9df5819c78cd556ed2f5a104628
melwin <114172287+itzTedx@users.noreply.github.com>
2025-07-21 21:26:57 +0400

    Enhance product detail page by adding tabs for product categories, incorporating datasheet download buttons, and improving layout with better spacing and formatting for readability.

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,13 +1,16 @@
 import Image from "next/image";
 
+import { IconChart, IconShield } from "@/assets/icons";
+import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { cn } from "@/lib/utils";
 
 export default function ProductSlugPage() {
   return (
     <main className="container py-8">
       <section className="grid grid-cols-2 gap-12">
-        <Card>
+        <Card className="h-fit">
           <CardContent className="relative aspect-square">
             <Image
               src="/images/xpower-hi.webp"
@@ -27,9 +30,9 @@ export default function ProductSlugPage() {
               XPower 5W30 Petrol Engine Oil (API SP)
             </h1>
           </div>
-          <div className="space-y-3">
-            <h2 className="text-muted-foreground">Product Overview</h2>
-            <p className="text-lg leading-normal tracking-tight">
+          <div>
+            <h2 className="text-muted-foreground mb-1">Product Overview</h2>
+            <p className="mb-3 text-lg leading-normal tracking-tight">
               Experience next-generation engine care with XPower 5W30,
               formulated to meet API SP standards. Designed for modern petrol
               engines, it delivers exceptional wear protection, cleaner
@@ -42,9 +45,9 @@ export default function ProductSlugPage() {
               quieter, and more efficiently every day.
             </p>
           </div>
-          <div className="space-y-3">
-            <h2 className="text-muted-foreground">Key Benefits</h2>
-            <ul className="list-disc text-lg">
+          <div>
+            <h2 className="text-muted-foreground mb-1">Key Benefits</h2>
+            <ul className="list-disc space-y-2 text-lg">
               <li className="ml-5"> Maximize Engine Potential</li>
               <li className="ml-5">
                 Quieter Engine, Smoother Operation, Enhanced Comfort
@@ -63,8 +66,67 @@ export default function ProductSlugPage() {
               </li>
             </ul>
           </div>
+          <div>
+            <h2 className="text-muted-foreground mb-1">Datasheet Downloads</h2>
+            <div className="space-x-3">
+              <Button variant="secondary">
+                <IconShield />
+                Material Safety Datasheet
+              </Button>
+              <Button variant="secondary">
+                <IconChart />
+                Technical Datasheet
+              </Button>
+            </div>
+          </div>
         </div>
       </section>
+      <Tabs defaultValue="all" className="items-center">
+        <TabsList className="text-foreground mb-6 h-auto w-full gap-2 rounded-none border-b bg-transparent px-0 py-1">
+          <TabsTrigger
+            value="all"
+            className="hover:bg-accent hover:text-foreground data-[state=active]:after:bg-primary data-[state=active]:hover:bg-accent relative flex-0 after:absolute after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5 data-[state=active]:bg-transparent data-[state=active]:shadow-none"
+          >
+            All Products
+          </TabsTrigger>
+          <TabsTrigger
+            value="lubrication"
+            className="hover:bg-accent hover:text-foreground data-[state=active]:after:bg-primary data-[state=active]:hover:bg-accent relative flex-0 after:absolute after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5 data-[state=active]:bg-transparent data-[state=active]:shadow-none"
+          >
+            Lubrication
+          </TabsTrigger>
+          <TabsTrigger
+            value="engine-additives"
+            className="hover:bg-accent hover:text-foreground data-[state=active]:after:bg-primary data-[state=active]:hover:bg-accent relative flex-0 after:absolute after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5 data-[state=active]:bg-transparent data-[state=active]:shadow-none"
+          >
+            Engine Additives
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="lubrication">
+          <div className="grid grid-cols-3 gap-4">
+            Superior Wear ProtectionForms a micro-layer on engine parts to
+            reduce friction and metal-to-metal contact, extending life and
+            ensuring smoother operation—especially in cold starts and high-load
+            scenarios.
+          </div>
+        </TabsContent>
+        <TabsContent value="all">
+          <div className="grid grid-cols-3 gap-4">
+            Superior Wear ProtectionForms a micro-layer on engine parts to
+            reduce friction and metal-to-metal contact, extending life and
+            ensuring smoother operation—especially in cold starts and high-load
+            scenarios.
+          </div>
+        </TabsContent>
+        <TabsContent value="engine-additives">
+          <div className="grid grid-cols-3 gap-4">
+            Superior Wear ProtectionForms a micro-layer on engine parts to
+            reduce friction and metal-to-metal contact, extending life and
+            ensuring smoother operation—especially in cold starts and high-load
+            scenarios.
+          </div>
+        </TabsContent>
+      </Tabs>
     </main>
   );
 }
