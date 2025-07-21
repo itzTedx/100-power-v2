aac1517e496eb45066bf86adb69514b586d149eb
melwin <114172287+itzTedx@users.noreply.github.com>
2025-07-21 21:38:55 +0400

    Refactor product detail page to enhance tab structure, adding new categories for Core Advantages, Usages, and Application. Improve content organization with detailed descriptions and usage directions for better user experience.

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -81,52 +81,143 @@ export default function ProductSlugPage() {
           </div>
         </div>
       </section>
-      <Tabs defaultValue="all" className="items-center">
-        <TabsList className="text-foreground mb-6 h-auto w-full gap-2 rounded-none border-b bg-transparent px-0 py-1">
-          <TabsTrigger
-            value="all"
-            className="hover:bg-accent hover:text-foreground data-[state=active]:after:bg-primary data-[state=active]:hover:bg-accent relative flex-0 after:absolute after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5 data-[state=active]:bg-transparent data-[state=active]:shadow-none"
-          >
-            All Products
-          </TabsTrigger>
-          <TabsTrigger
-            value="lubrication"
-            className="hover:bg-accent hover:text-foreground data-[state=active]:after:bg-primary data-[state=active]:hover:bg-accent relative flex-0 after:absolute after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5 data-[state=active]:bg-transparent data-[state=active]:shadow-none"
-          >
-            Lubrication
-          </TabsTrigger>
-          <TabsTrigger
-            value="engine-additives"
-            className="hover:bg-accent hover:text-foreground data-[state=active]:after:bg-primary data-[state=active]:hover:bg-accent relative flex-0 after:absolute after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5 data-[state=active]:bg-transparent data-[state=active]:shadow-none"
-          >
-            Engine Additives
-          </TabsTrigger>
-        </TabsList>
-        <TabsContent value="lubrication">
-          <div className="grid grid-cols-3 gap-4">
-            Superior Wear ProtectionForms a micro-layer on engine parts to
-            reduce friction and metal-to-metal contact, extending life and
-            ensuring smoother operation—especially in cold starts and high-load
-            scenarios.
-          </div>
-        </TabsContent>
-        <TabsContent value="all">
-          <div className="grid grid-cols-3 gap-4">
-            Superior Wear ProtectionForms a micro-layer on engine parts to
-            reduce friction and metal-to-metal contact, extending life and
-            ensuring smoother operation—especially in cold starts and high-load
-            scenarios.
-          </div>
-        </TabsContent>
-        <TabsContent value="engine-additives">
-          <div className="grid grid-cols-3 gap-4">
-            Superior Wear ProtectionForms a micro-layer on engine parts to
-            reduce friction and metal-to-metal contact, extending life and
-            ensuring smoother operation—especially in cold starts and high-load
-            scenarios.
-          </div>
-        </TabsContent>
-      </Tabs>
+      <div className="mt-8 grid grid-cols-12 gap-4">
+        <Tabs defaultValue="core" className="col-span-7 items-start">
+          <TabsList className="text-foreground mb-3 h-auto w-full items-start gap-2 rounded-none border-b bg-transparent px-0 py-1">
+            <TabsTrigger
+              value="core"
+              className="hover:bg-accent hover:text-foreground data-[state=active]:after:bg-primary data-[state=active]:hover:bg-accent relative flex-0 after:absolute after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5 data-[state=active]:bg-transparent data-[state=active]:shadow-none"
+            >
+              Core Advantages
+            </TabsTrigger>
+            <TabsTrigger
+              value="usages"
+              className="hover:bg-accent hover:text-foreground data-[state=active]:after:bg-primary data-[state=active]:hover:bg-accent relative flex-0 after:absolute after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5 data-[state=active]:bg-transparent data-[state=active]:shadow-none"
+            >
+              Usages
+            </TabsTrigger>
+            <TabsTrigger
+              value="application"
+              className="hover:bg-accent hover:text-foreground data-[state=active]:after:bg-primary data-[state=active]:hover:bg-accent relative flex-0 after:absolute after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5 data-[state=active]:bg-transparent data-[state=active]:shadow-none"
+            >
+              Application
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="core">
+            <ul className="ml-5 list-disc space-y-3">
+              <li>
+                <h3 className="text-xl">
+                  <strong>Superior Wear Protection</strong>
+                </h3>
+                <p>
+                  Forms a micro-layer on engine parts to reduce friction and
+                  metal-to-metal contact, extending life and ensuring smoother
+                  operation—especially in cold starts and high-load scenarios.
+                </p>
+              </li>
+              <li>
+                <h3 className="text-xl">
+                  <strong>Enhanced Engine Efficiency</strong>
+                </h3>
+                <p>
+                  Minimizes internal drag, improving fuel economy, acceleration,
+                  and engine responsiveness.
+                </p>
+              </li>
+              <li>
+                <h3 className="text-xl">
+                  <strong>Exceptional Cleanliness</strong>
+                </h3>
+                <p>
+                  Advanced detergents remove sludge, varnish, and deposits to
+                  keep your engine running at its peak.
+                </p>
+              </li>
+              <li>
+                <h3 className="text-xl">
+                  <strong>Reliable Cold Start Protection</strong>
+                </h3>
+                <p>
+                  Instant lubrication at startup reduces early-stage wear and
+                  enhances overall reliability.
+                </p>
+              </li>
+              <li>
+                <h3 className="text-xl">
+                  <strong>Cooler Operating Temperatures</strong>
+                </h3>
+                <p>
+                  Reduces friction heat, allowing your engine to perform
+                  consistently under harsh conditions.
+                </p>
+              </li>
+            </ul>
+          </TabsContent>
+          <TabsContent value="usages">
+            <div className="grid gap-4">
+              Superior Wear ProtectionForms a micro-layer on engine parts to
+              reduce friction and metal-to-metal contact, extending life and
+              ensuring smoother operation—especially in cold starts and
+              high-load scenarios.
+            </div>
+          </TabsContent>
+          <TabsContent value="application">
+            <div className="grid gap-4">
+              Superior Wear ProtectionForms a micro-layer on engine parts to
+              reduce friction and metal-to-metal contact, extending life and
+              ensuring smoother operation—especially in cold starts and
+              high-load scenarios.
+            </div>
+          </TabsContent>
+        </Tabs>
+        <Tabs defaultValue="directions" className="col-span-5 items-start">
+          <TabsList className="text-foreground mb-3 h-auto w-full items-start gap-2 rounded-none border-b bg-transparent px-0 py-1">
+            <TabsTrigger
+              value="directions"
+              className="hover:bg-accent hover:text-foreground data-[state=active]:after:bg-primary data-[state=active]:hover:bg-accent relative flex-0 after:absolute after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5 data-[state=active]:bg-transparent data-[state=active]:shadow-none"
+            >
+              Directions of Use
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="directions">
+            <ol className="ml-5 list-decimal space-y-3">
+              <li>
+                <strong>Check Compatibility</strong>
+                <p>Refer to your vehicle’s manual to confirm suitability.</p>
+              </li>
+              <li>
+                <strong>Drain Old Oil</strong>
+                <p>Ensure complete drainage to avoid contamination.</p>
+              </li>
+              <li>
+                <strong>Replace Oil Filter</strong>
+                <p>
+                  Always install a new filter to maximize oil effectiveness.
+                </p>
+              </li>
+              <li>
+                <strong>Pour the Oil</strong>
+                <p>Add XPower 5W30 as per the recommended capacity.</p>
+              </li>
+              <li>
+                <strong>Check Level</strong>
+                <p>Use a dipstick and top up if required.</p>
+              </li>
+              <li>
+                <strong>Start Engine & Inspect</strong>
+                <p>Let it run for a few minutes. Check for any leaks.</p>
+              </li>
+              <li>
+                <strong>Regular Maintenance</strong>
+                <p>
+                  Monitor levels periodically and follow recommended change
+                  intervals.
+                </p>
+              </li>
+            </ol>
+          </TabsContent>
+        </Tabs>
+      </div>
     </main>
   );
 }
