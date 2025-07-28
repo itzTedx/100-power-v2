4e66a0b06e030d336f435d3653cb9fc6044d5baf
melwin <114172287+itzTedx@users.noreply.github.com>
2025-07-28 15:03:57 +0400

    refactor: update Tablist component background for improved aesthetics
    - Changed the background of the Tablist component from a solid red to a gradient for enhanced visual appeal.
    - Maintained existing layout and functionality while improving the overall design consistency.

--- src/features/products/components/tab-list.tsx
+++ src/features/products/components/tab-list.tsx
@@ -53,7 +53,7 @@ export const Tablist = ({ children }: { children: ReactNode }) => {
           >
             Industrial Application
           </TabsTrigger>
-          <div className="absolute bg-red-200 inset-0" />
+          <div className="absolute bg-gradient-to-r inset-0" />
         </TabsList>
         <ScrollBar orientation="horizontal" />
       </ScrollArea>
